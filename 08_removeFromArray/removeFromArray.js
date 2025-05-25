9fa185af5e04a7d1905249eab425c2c2e2585470
f-azumah <fiona.azumah@gmail.com>
2025-05-25 19:03:33 -0400

    xolution ex08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const result = [];
+    array.forEach(element => {
+        if(!args.includes(element)){
+            result.push(element);
+        }
+    });
+
+    return result
 };
 
 // Do not edit below this line
